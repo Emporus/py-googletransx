9aeb8ab3132376e725a843608de5ae9d08d4becc
x0rzkov <x0rzkov@gmail.com>
2019-11-24 13:34:43 +0100

    create pypi googletrans2

--- setup.py
+++ setup.py
@@ -35,14 +35,14 @@ def get_readme():
 
 def install():
     setup(
-        name='googletrans',
+        name='googletrans2',
         version=get_version(),
         description=get_description(),
         long_description=get_readme(),
         license='MIT',
-        author='SuHun Han',
-        author_email='ssut' '@' 'ssut.me',
-        url='https://github.com/ssut/py-googletrans',
+        author='x0rxkov',
+        author_email='x0rxkov' '@' 'gmail.com',
+        url='https://github.com/x0rkov/py-googletrans',
         classifiers=['Development Status :: 5 - Production/Stable',
                      'Intended Audience :: Education',
                      'Intended Audience :: End Users/Desktop',
