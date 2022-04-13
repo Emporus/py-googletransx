6712bdf6d21eda3c67063bd3d0db37a8746d8325
Slava Shor <slava.shor@emporus.com>
2022-04-13 17:00:25 +0300

    Bump version: 2.4.3 → 2.4.4

--- googletransx/__init__.py
+++ googletransx/__init__.py
@@ -2,7 +2,7 @@
 Free Google Translate API for Python. Translates totally free of charge.
 """
 __all__ = ["Translator"],
-__version__ = "2.4.3"
+__version__ = "2.4.4"
 
 from googletransx.client import Translator
 from googletransx.constants import LANGCODES, LANGUAGES
